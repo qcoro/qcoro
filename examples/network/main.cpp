f3e2e16cf245e96662bdd5f6e62106513a25f925
Daniel Vrátil <dvratil@kde.org>
2021-03-24 23:24:20 +0100

    Add awaiter for QNetworkReply

---
+++ examples/network/main.cpp
@@ -0,0 +1,87 @@
+// SPDX-FileCopyrightText: 2021 Daniel Vrátil <dvratil@kde.org>
+//
+// SPDX-License-Identifier: MIT
+
+#include "qcoro/task.h"
+#include "qcoro/network.h"
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QBoxLayout>
+#include <QProgressBar>
+#include <QPushButton>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QMessageBox>
+
+#include <memory>
+
+// Programming langauges
+static const QUrl wikiUrl = QUrl{QStringLiteral("https://www.wikidata.org/wiki/Special:EntityData/Q9143.json")};
+
+class MainWindow: public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow() {
+        mPb = new QProgressBar();
+        mPb->setVisible(false);
+        mPb->setMinimumWidth(200);
+        mPb->setMinimum(0);
+        mPb->setMaximum(0);
+
+        mBtn = new QPushButton(tr("Start Download"));
+        connect(mBtn, &QPushButton::clicked,
+                this, &MainWindow::start);
+
+        auto *vbox = new QVBoxLayout();
+        vbox->addStretch(1);
+        vbox->addWidget(mPb);
+        vbox->addWidget(mBtn);
+        vbox->addStretch(1);
+
+        auto *hbox = new QHBoxLayout();
+        hbox->addStretch(1);
+        hbox->addLayout(vbox);
+        hbox->addStretch(1);
+
+        QWidget *w = new QWidget;
+        w->setLayout(hbox);
+        setCentralWidget(w);
+    }
+
+private Q_SLOTS:
+    QCoro::Task<> start() {
+        mPb->setVisible(true);
+        mBtn->setEnabled(false);
+        mBtn->setText(tr("Downloading ..."));
+
+        auto *reply = co_await mNam.get(QNetworkRequest{wikiUrl});
+        if (reply->error()) {
+            QMessageBox::warning(this,
+                tr("Network request error"),
+                tr("Error occured during network request. Error code: %1").arg(reply->error()));
+            co_return;
+        }
+        delete reply;
+
+        mPb->setVisible(false);
+        mBtn->setEnabled(true);
+        mBtn->setText(tr("Done, download again"));
+    }
+
+private:
+    QNetworkAccessManager mNam;
+    QPushButton *mBtn = {};
+    QProgressBar *mPb = {};
+};
+
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.showNormal();
+    return app.exec();
+}
+
+#include "main.moc"
---
+++ qcoro/network.h
@@ -0,0 +1,47 @@
+// SPDX-FileCopyrightText: 2021 Daniel Vrátil <dvratil@kde.org>
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <QPointer>
+#include <QNetworkReply>
+
+namespace QCoro::detail
+{
+
+class NetworkReplyAwaiter {
+public:
+    explicit NetworkReplyAwaiter(QNetworkReply *reply)
+        : mReply(reply)
+    {}
+
+    bool await_ready() const noexcept {
+        return !mReply || mReply->isFinished();
+    }
+
+    void await_suspend(QCORO_STD::coroutine_handle<> awaitingCoroutine) {
+        if (mReply) {
+            QObject::connect(mReply, &QNetworkReply::finished,
+                    [awaitingCoroutine]() mutable {
+                        awaitingCoroutine.resume();
+                    });
+        } else {
+            awaitingCoroutine.resume();
+        }
+    }
+
+    QNetworkReply *await_resume() const {
+        return mReply;
+    }
+
+private:
+    QPointer<QNetworkReply> mReply;
+};
+
+template<>
+struct awaiter_type<QNetworkReply *> {
+    using type = NetworkReplyAwaiter;
+};
+
+} // namespace QCoro::detail
