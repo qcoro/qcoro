178d2f45fa95a91d40318ccec658bc438291f6a5
Richard Yu <yurichard3839@gmail.com>
2021-07-01 18:51:05 +0800

    Add support for MSVC

--- QCoroMacros.cmake
+++ QCoroMacros.cmake
@@ -4,7 +4,7 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines")
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines-ts -stdlib=libc++")
-else()
+elseif (NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
     message(FATAL_ERROR "Compiler ${CMAKE_CXX_COMPILER_ID} is not currently supported.")
 endif()
 
