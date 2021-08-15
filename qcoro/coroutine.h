08d2929762734104b3b4825a3e3c7f3b581c79c8
Daniel Vrátil <dvratil@kde.org>
2021-08-15 14:28:22 +0200

    Fix the Awaitable concept not matching types with 'operator co_await' overload
    Fixes issue #9

--- qcoro/coroutine.h
+++ qcoro/coroutine.h
@@ -20,17 +20,31 @@
 
 namespace QCoro {
 
+namespace detail {
+
+template<typename T>
+concept has_await_methods = requires(T t) {
+    { t.await_ready() } -> std::same_as<bool>;
+    {t.await_suspend(std::declval<QCORO_STD::coroutine_handle<>>())};
+    {t.await_resume()};
+};
+
+template<typename T>
+concept has_operator_coawait = requires(T t) {
+    // TODO: Check that result of co_await() satisfies Awaitable again
+    { t.operator co_await() };
+};
+
+
+} // namespace detail
+
 //! A concept describing the Awaitable type
 /*!
  * Awaitable type is a type that can be passed as an argument to co_await.
  */
 template<typename T>
-concept Awaitable = requires(T t) {
-    { t.await_ready() }
-    ->std::same_as<bool>;
-    {t.await_suspend(std::declval<QCORO_STD::coroutine_handle<>>())};
-    {t.await_resume()};
-};
+concept Awaitable = detail::has_operator_coawait<T> ||
+                    detail::has_await_methods<T>;
 
 } // namespace QCoro
 
---
+++ tests/testconstraints.cpp
@@ -0,0 +1,40 @@
+#include <QObject>
+#include <QTest>
+
+#include "../qcoro/coroutine.h"
+#include "../qcoro/task.h"
+
+namespace helper {
+
+template<QCoro::Awaitable T>
+struct is_awaitable {
+    static constexpr bool value = true;
+};
+
+template<QCoro::Awaitable T>
+static constexpr bool is_awaitable_v = is_awaitable<T>::value;
+
+}
+
+struct TestAwaitable {
+    bool await_ready() const { return true; }
+    void await_suspend(QCORO_STD::coroutine_handle<>);
+    void await_resume() const;
+};
+
+
+class TestConstraints : public QObject {
+    Q_OBJECT
+private Q_SLOTS:
+    void testAwaitableConcept() {
+
+        static_assert(helper::is_awaitable_v<QCoro::Task<void>>,
+                      "Awaitable concept doesn't accept Task<T>, although it should.");
+        static_assert(helper::is_awaitable_v<TestAwaitable>,
+                      "Awaitable concept doesn't accept TestAwaitable, although it should.");
+    }
+};
+
+QTEST_GUILESS_MAIN(TestConstraints)
+
+#include "testconstraints.moc"
