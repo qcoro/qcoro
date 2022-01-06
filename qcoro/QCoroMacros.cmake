f65384a7797a9f321dd47b24664494eb2585a6ea
Daniel Vrátil <dvratil@kde.org>
2022-01-06 15:49:06 +0100

    Allow libstdc++ when building with Clang
    Since we now provide our own implementaion of the standard <coroutine>
    header for Clang, we are no longer limited by Clang looking for
    coroutines in std::experimental namespace, which was only provided by
    libc++.
    
    libc++ is still supported with Clang, but we no longer provide the hack
    to support mixing libc++-built QCoro with libstd++-built Qt.

--- qcoro/QCoroMacros.cmake
+++ qcoro/QCoroMacros.cmake
@@ -1,11 +1,11 @@
 macro(qcoro_enable_coroutines)
-
-if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines")
-elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines-ts -stdlib=libc++")
-elseif (NOT CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    message(FATAL_ERROR "Compiler ${CMAKE_CXX_COMPILER_ID} is not currently supported.")
-endif()
-
+    if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines")
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcoroutines-ts")
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        # MSVC auto-enables coroutine support when C++20 is enabled
+    else()
+        message(FATAL_ERROR "Compiler ${CMAKE_CXX_COMPILER_ID} is not currently supported.")
+    endif()
 endmacro()
