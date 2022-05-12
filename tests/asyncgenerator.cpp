2158b31d643127151d1457d5ae533e5038fb38b5
Daniel Vrátil <dvratil@kde.org>
2022-05-12 15:21:57 +0200

    Extend AsyncGenerator tests

--- tests/asyncgenerator.cpp
+++ tests/asyncgenerator.cpp
@@ -8,6 +8,8 @@
 
 #include <vector>
 
+#include <QScopeGuard>
+
 class AsyncGeneratorTest : public QCoro::TestObject<AsyncGeneratorTest> {
     Q_OBJECT
 private:
@@ -29,8 +31,67 @@ private:
         QCORO_COMPARE(values, (std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
     }
 
+    QCoro::Task<> testSyncGenerator_coro(QCoro::TestContext ctx) {
+        ctx.setShouldNotSuspend();
+
+        const auto createGenerator = []() -> QCoro::AsyncGenerator<int> {
+            for (int i = 0; i < 10; ++i) {
+                co_yield i;
+            }
+        };
+
+        std::vector<int> values;
+        QCORO_FOREACH(int val, createGenerator()) {
+            values.push_back(val);
+        }
+
+        QCORO_COMPARE(values, (std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
+    }
+
+    QCoro::Task<> testTerminateSuspendedGenerator_coro(QCoro::TestContext ctx) {
+        ctx.setShouldNotSuspend();
+
+        bool destroyed = false;
+        const auto createGenerator = [&destroyed]() -> QCoro::AsyncGenerator<int> {
+            const auto guard = qScopeGuard([&destroyed]() {
+                destroyed = true;
+            });
+
+            const auto pointer = std::make_unique<QString>(
+                QStringLiteral("This should get destroyed. If not, ASAN will catch it."));
+
+            while (true) {
+                co_yield 42;
+            }
+        };
+
+        {
+            auto generator = createGenerator();
+            const auto it = co_await generator.begin();
+            QCORO_COMPARE(*it, 42);
+        } // The generator gets destroyed here. Everything on generator's stack is destroyed.
+
+        QCORO_VERIFY(destroyed);
+    }
+
+    QCoro::Task<> testEmptyGenerator_coro(QCoro::TestContext ctx) {
+        ctx.setShouldNotSuspend();
+
+        const auto createGenerator = []() -> QCoro::AsyncGenerator<int> {
+            if (false) {
+                co_yield 42;
+            }
+        };
+
+        auto generator = createGenerator();
+        QCORO_COMPARE(co_await generator.begin(), generator.end());
+    }
+
 private Q_SLOTS:
     addTest(Generator)
+    addTest(SyncGenerator)
+    addTest(TerminateSuspendedGenerator)
+    addTest(EmptyGenerator)
 };
 
 QTEST_GUILESS_MAIN(AsyncGeneratorTest)
