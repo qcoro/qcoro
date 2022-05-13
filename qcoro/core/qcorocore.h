6627ff85f02d3441e04ffb79043c1bc05a3d5a6f
Daniel Vrátil <dvratil@kde.org>
2022-05-13 14:37:56 +0200

    Fix qcorocore.h header
    The config.h file doesn't exist in main branch, and the QFuture<> ABI
    incompatibility has been solved by using libstdc++ even in clang builds,
    except for apple-clang, but there we have Qt built with libc++ as well
    so ABI compatibility is ensured.

--- qcoro/core/qcorocore.h
+++ qcoro/core/qcorocore.h
@@ -2,14 +2,9 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include "config.h"
-
 #include "qcoroiodevice.h"
 #include "qcoroprocess.h"
 #include "qcorosignal.h"
 #include "qcorotimer.h"
-
-#ifdef QCORO_QT_HAS_COMPAT_ABI
-    #include "qcorofuture.h"
-#endif
+#include "qcorofuture.h"
 
