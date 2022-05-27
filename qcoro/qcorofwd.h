06e92aecc10014952028539839cd1486897b4bf0
Daniel Vrátil <dvratil@kde.org>
2022-05-27 22:21:36 +0200

    Add QCoroFwd header with forward-declarations of QCoro types (#71)

---
+++ qcoro/qcorofwd.h
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2022 Daniel Vrátil <dvratil@kde.org>
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace QCoro {
+
+template<typename T> class Task;
+template<typename T> class Generator;
+template<typename T> class GeneratorIterator;
+template<typename T> class AsyncGenerator;
+template<typename T> class AsyncGeneratorIterator<T>;
+
+} // namespace QCoro
