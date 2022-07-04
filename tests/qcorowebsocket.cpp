3c3943146ee745af9f7cc9b3a5278d2ce1cfd65a
Daniel Vrátil <dvratil@kde.org>
2022-07-04 17:12:50 +0200

    Tests: refactor QCoroWebSocketTest to reduce code duplication

--- tests/qcorowebsocket.cpp
+++ tests/qcorowebsocket.cpp
@@ -12,6 +12,39 @@ class QCoroWebSocketTest : public QCoro::TestObject<QCoroWebSocketTest> {
     Q_OBJECT
 
 private:
+    template<typename T, typename SendFunc, typename RecvFunc>
+    QCoro::Task<> testReceived(const T &msg, SendFunc sendFunc, RecvFunc recvFunc) {
+        QWebSocket socket;
+        QCORO_VERIFY(connectSocket(socket));
+
+        QCORO_DELAY(std::invoke(sendFunc, socket, msg));
+        auto coroSocket = qCoro(socket);
+        auto gen = std::invoke(recvFunc, &coroSocket, std::chrono::milliseconds{-1});
+        const auto data = co_await gen.begin();
+        QCORO_VERIFY(data != gen.end());
+        if constexpr (std::is_same_v<std::remove_cvref_t<decltype(*data)>, QString> ||
+                      std::is_same_v<std::remove_cvref_t<decltype(*data)>, QByteArray>) {
+            QCORO_COMPARE(*data, msg);
+        } else {
+            QCORO_COMPARE(std::get<0>(*data), msg);
+            QCORO_COMPARE(std::get<1>(*data), true);
+        }
+    }
+
+    template<typename RecvFunc>
+    QCoro::Task<> testTimeout(RecvFunc recvFunc) {
+        mServer.setExpectTimeout();
+
+        QWebSocket socket;
+        QCORO_VERIFY(connectSocket(socket));
+
+        auto coroSocket = qCoro(socket);
+        auto gen = std::invoke(recvFunc, &coroSocket, 10ms);
+        const auto data = co_await gen.begin();
+        QCORO_COMPARE(data, gen.end());
+    }
+
+private:
     QCoro::Task<> testWaitForOpenWithUrl_coro(QCoro::TestContext) {
         QWebSocket socket;
         const auto result = co_await qCoro(socket).open(mServer.url());
@@ -115,93 +148,39 @@ private:
     }
 
     QCoro::Task<> testBinaryFrame_coro(QCoro::TestContext) {
-        QWebSocket socket;
-        QCORO_VERIFY(connectSocket(socket));
-
-        QCORO_DELAY(socket.sendBinaryMessage("TEST MESSAGE"));
-        auto frames = qCoro(socket).binaryFrames();
-        const auto frame = co_await frames.begin();
-        QCORO_VERIFY(frame != frames.end());
-        QCORO_COMPARE(std::get<0>(*frame), QByteArray("TEST MESSAGE"));
-        QCORO_COMPARE(std::get<1>(*frame), true);
+        co_await testReceived(QByteArray("TEST MESSAGE"), &QWebSocket::sendBinaryMessage,
+                              &QCoro::detail::QCoroWebSocket::binaryFrames);
     }
 
     QCoro::Task<> testBinaryFrameTimeout_coro(QCoro::TestContext) {
-        mServer.setExpectTimeout();
-
-        QWebSocket socket;
-        QCORO_VERIFY(connectSocket(socket));
-
-        auto frames = qCoro(socket).binaryFrames(10ms);
-        const auto frame = co_await frames.begin();
-        QCORO_COMPARE(frame, frames.end());
+        co_await testTimeout(&QCoro::detail::QCoroWebSocket::binaryFrames);
     }
 
     QCoro::Task<> testBinaryMessage_coro(QCoro::TestContext) {
-        QWebSocket socket;
-        QCORO_VERIFY(connectSocket(socket));
-
-        QCORO_DELAY(socket.sendBinaryMessage("TEST MESSAGE"));
-        auto messages = qCoro(socket).binaryMessages();
-        const auto message = co_await messages.begin();
-        QCORO_VERIFY(message != messages.end());
-        QCORO_COMPARE(*message, QByteArray("TEST MESSAGE"));
+        co_await testReceived(QByteArray("TEST MESSAGE"), &QWebSocket::sendBinaryMessage,
+                              &QCoro::detail::QCoroWebSocket::binaryMessages);
     }
 
     QCoro::Task<> testBinaryMessageTimeout_coro(QCoro::TestContext) {
-        mServer.setExpectTimeout();
-
-        QWebSocket socket;
-        QCORO_VERIFY(connectSocket(socket));
-
-        auto messages = qCoro(socket).binaryMessages(10ms);
-        const auto message = co_await messages.begin();
-        QCORO_COMPARE(message, messages.end());
+        co_await testTimeout(&QCoro::detail::QCoroWebSocket::binaryMessages);
     }
 
     QCoro::Task<> testTextFrame_coro(QCoro::TestContext) {
-        QWebSocket socket;
-        QCORO_VERIFY(connectSocket(socket));
-
-        QCORO_DELAY(socket.sendTextMessage(QStringLiteral("TEST MESSAGE")));
-        auto frames = qCoro(socket).textFrames();
-        const auto frame = co_await frames.begin();
-        QCORO_VERIFY(frame != frames.end());
-        QCORO_COMPARE(std::get<0>(*frame), QStringLiteral("TEST MESSAGE"));
-        QCORO_COMPARE(std::get<1>(*frame), true);
+        co_await testReceived(QStringLiteral("TEST MESSAGE"), &QWebSocket::sendTextMessage,
+                             &QCoro::detail::QCoroWebSocket::textFrames);
     }
 
     QCoro::Task<> testTextFrameTimeout_coro(QCoro::TestContext) {
-        mServer.setExpectTimeout();
-
-        QWebSocket socket;
-        QCORO_VERIFY(connectSocket(socket));
-
-        auto frames = qCoro(socket).textFrames(10ms);
-        const auto frame = co_await frames.begin();
-        QCORO_COMPARE(frame, frames.end());
+        co_await testTimeout(&QCoro::detail::QCoroWebSocket::textFrames);
     }
 
     QCoro::Task<> testTextMessage_coro(QCoro::TestContext) {
-        QWebSocket socket;
-        QCORO_VERIFY(connectSocket(socket));
-
-        QCORO_DELAY(socket.sendTextMessage(QStringLiteral("TEST MESSAGE")));
-        auto messages = qCoro(socket).textMessages();
-        const auto message = co_await messages.begin();
-        QCORO_VERIFY(message != messages.end());
-        QCORO_COMPARE(*message, QStringLiteral("TEST MESSAGE"));
+        co_await testReceived(QStringLiteral("TEST MESSAGE"), &QWebSocket::sendTextMessage,
+                              &QCoro::detail::QCoroWebSocket::textMessages);
     }
 
     QCoro::Task<> testTextMessageTimeout_coro(QCoro::TestContext) {
-        mServer.setExpectTimeout();
-
-        QWebSocket socket;
-        QCORO_VERIFY(connectSocket(socket));
-
-        auto messages = qCoro(socket).textMessages(10ms);
-        const auto message = co_await messages.begin();
-        QCORO_COMPARE(message, messages.end());
+        co_await testTimeout(&QCoro::detail::QCoroWebSocket::textMessages);
     }
 
     QCoro::Task<> testReadFragmentedMessage_coro(QCoro::TestContext) {
