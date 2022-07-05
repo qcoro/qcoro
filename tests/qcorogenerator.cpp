6ce2e2ac183238c16348822077d7079fa926a91f
Daniel Vrátil <dvratil@kde.org>
2022-07-05 15:51:33 +0200

    Disable ASAN with clang-11 completely
    Something in generators crashes ASAN. The codegen for generators has
    changed completely in clang 12 and there are no longer any issues, so
    ASAN support has been disabled only for clang<=11

--- tests/qcorogenerator.cpp
+++ tests/qcorogenerator.cpp
@@ -9,19 +9,6 @@
 #include <QTest>
 #include <QScopeGuard>
 
-
-// In clang <= 11 ASAN detects stack-use-after-scope in values.emplace_back(*it).
-// I believe this is a bug in clang, since the codegen has change in clang 12 and
-// the issue is no longer present. GCC and MSVC produce totally different assembly
-// and the issue doesn't manifest there either.
-// FIXME: Drop this once we bump minimum clang version to 12
-// FIXME: Prove that this is really a bug in clang and try to workaround it.
-#if defined(__clang__) && __clang_major__ <= 11
-    #define CLANG11_DISABLE_ASAN __attribute__((no_sanitize("address")))
-#else
-    #define CLANG11_DISABLE_ASAN
-#endif
-
 struct Nocopymove {
     explicit constexpr Nocopymove(int val): val(val) {}
     Nocopymove(const Nocopymove &) = delete;
@@ -48,7 +35,7 @@ class GeneratorTest : public QObject {
     Q_OBJECT
 private Q_SLOTS:
 
-    CLANG11_DISABLE_ASAN void testImmediateGenerator() {
+    void testImmediateGenerator() {
         const auto createGenerator = []() -> QCoro::Generator<int> {
             for (int value = 0; value < 10; ++value) {
                 co_yield value;
@@ -65,7 +52,7 @@ private Q_SLOTS:
         QCOMPARE(values, (std::vector<int>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}));
     }
 
-    CLANG11_DISABLE_ASAN void testTerminateSuspendedGenerator() {
+    void testTerminateSuspendedGenerator() {
         bool destroyed = false;
         const auto createGenerator = [&destroyed]() -> QCoro::Generator<int> {
             const auto guard = qScopeGuard([&destroyed]() {
@@ -89,7 +76,7 @@ private Q_SLOTS:
         QVERIFY(destroyed);
     }
 
-    CLANG11_DISABLE_ASAN void testEmptyGenerator() {
+    void testEmptyGenerator() {
         const auto createGenerator = []() -> QCoro::Generator<int> {
             if (false) { // NOLINT(readability-simplify-boolean-expr)
                 co_yield 42; // Make it a coroutine, except it never gets invoked.
@@ -101,7 +88,7 @@ private Q_SLOTS:
         QCOMPARE(begin, generator.end());
     }
 
-    CLANG11_DISABLE_ASAN void testConstReferenceGenerator() {
+    void testConstReferenceGenerator() {
         const auto createGenerator = []() -> QCoro::Generator<const Nocopymove &> {
             for (int i = 0; i < 4; ++i) {
                 const Nocopymove val(i);
@@ -120,7 +107,7 @@ private Q_SLOTS:
         QCOMPARE(testval, 4);
     }
 
-    CLANG11_DISABLE_ASAN void testReferenceGenerator() {
+    void testReferenceGenerator() {
         const auto createGenerator = []() -> QCoro::Generator<Nocopymove &> {
             for (int i = 0; i < 8; i += 2) {
                 Nocopymove val(i);
@@ -141,7 +128,7 @@ private Q_SLOTS:
         }
     }
 
-    CLANG11_DISABLE_ASAN void testMoveonlyGenerator() {
+    void testMoveonlyGenerator() {
         const auto createGenerator = []() -> QCoro::Generator<Moveonly &&> {
             for (int i = 0; i < 4; ++i) {
                 Moveonly value{i};
