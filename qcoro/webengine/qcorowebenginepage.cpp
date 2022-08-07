7bdadb59d5d3e9f795199bf20b2f2c07a80bb5be
Daniel Vrátil <dvratil@kde.org>
2022-08-07 21:33:52 +0200

    Finish Qt5/Qt6 implementation of QWebEnginePage support

--- qcoro/webengine/qcorowebenginepage.cpp
+++ qcoro/webengine/qcorowebenginepage.cpp
@@ -1,6 +1,9 @@
 #include "qcorowebenginepage.h"
 #include "qcorosignal.h"
 
+#include <QAction>
+#include <QWebEngineProfile>
+
 using namespace QCoro::detail;
 
 namespace {
@@ -21,6 +24,27 @@ private:
     T mResult;
 };
 
+template<typename T>
+QCoro::Task<> savePage(QWebEnginePage *page, T::SavePageFormat saveFormat) {
+    auto downloadStart = qCoro(mPage->profile(), &QWebEngineProfile::downloadRequested);
+    mPage->action(QWebEnginePage::SavePage)->trigger();
+    auto *download = co_await downloadStart;
+    auto downloadFinished = qCoro(download, &T::finished);
+    download->setSavePageFormat(format);
+    switch (format) {
+        case T::UnknownSaveFormat:
+        case T::SingleHtmlSaveFormat:
+        case T::MimeHtmlSaveFormat:
+            download->setDownloadFileName(filePath);
+            break;
+        case T::CompleteHtmlSaveFormat:
+            download->setDownloadDirectory(filePath);
+            break;
+    }
+    download->accept();
+    co_await downloadFinished;
+}
+
 } // namespace
 
 QCoroWebEnginePage::QCoroWebEnginePage(QWebEnginePage *page)
@@ -28,24 +52,53 @@ QCoroWebEnginePage::QCoroWebEnginePage(QWebEnginePage *page)
 {}
 
 QCoro::Task<QWebEngineFindTextResult> QCoroWebEnginePage::findText(const QString &subString, QWebEnginePage::FindFlags options) {
-    WebEngineAwaitable<QWebEngineFindTextResult> awaitable;
-    mPage->findText(subString, options, [&awaitable](const auto &result) {
+    auto findFinished = qCoro(mPage.data(), &QWebEnginePage::findTextFinished);
+    mPage->findText(subString, options);
+    co_return co_await findFinished;
+}
+
+QCoro::Task<QVariant> QCoroWebEnginePage::runJavaScript(const QString &scriptSource) {
+    WebEngineAwaitable<QVariant> awaitable;
+    mPage->runJavaScript(scriptSource, [&awaitable](const QVariant &result) {
         awaitable.resume(result);
     });
+    co_return co_await awaitable;
+}
 
+QCoro::Task<QVariant> QCoroWebEnginePage::runJavaScript(const QString &scriptSource, quint32 worldId) {
+    WebEngineAwaitable<QVariant> awaitable;
+    mPage->runJavaScript(scriptSource, worldId, [&awaitable](const QVariant &result) {
+        awaitable.resume(result);
+    });
+    co_return co_await awaitable;
+}
+
+QCoro::Task<QString> QCoroWebEnginePage::toHtml() const {
+    WebEngineAwaitable<QString> awaitable;
+    mPage->toHtml([&awaitable](const QString &result) {
+        awaitable.resume(result);
+    });
+    co_return co_await awaitable;
+}
+
+QCoro::Task<QString> QCoroWebEnginePage::toPlainText() const {
+    WebEngineAwaitable<QString> awaitable;
+    mPage->toPlainText([&awaitable](const QString &result) {
+        awaitable.resume(result);
+    });
     co_return co_await awaitable;
 }
 
 #if QT_VERSION_MAJOR == 5
 
 QCoro::Task<bool> QCoroWebEnginePage::load(const QUrl &url) {
-    auto result = qCoro(mPage, &QWebEnginePage::loadFinished);
+    auto result = qCoro(mPage.data(), &QWebEnginePage::loadFinished);
     mPage->load(url);
     return result;
 }
 
 QCoro::Task<bool> QCoroWebEnginePage::load(const QWebEngineHttpRequest &request) {
-    auto result = qCoro(mPage, &QWebEnginePage::loadFinished);
+    auto result = qCoro(mPage.data(), &QWebEnginePage::loadFinished);
     mPage->load(request);
     return result;
 }
@@ -61,16 +114,15 @@ QCoro::Task<bool> QCoroWebEnginePage::print(QPrinter *printer) {
 
 QCoro::Task<QByteArray> QCoroWebEnginePage::printToPdf(const QPageLayout &layout) {
     WebEngineAwaitable<QByteArray> awaitable;
-    mPage->printToPdf(layout, [&awaitable](const QByteArray &result) {
+    mPage->printToPdf([&awaitable](const QByteArray &result) {
         awaitable.resume(result);
-    });
+    }, layout);
 
     co_return co_await awaitable;
 }
 
-QCoro::Task<> QCoroWebEnginePage::save(const QString &filePath, QWebEngineDownloadItem::SavePageFormat format) {
-    mPage->save(filePath, format);
-
+QCoro::Task<> QCoroWebEnginePage::save(const QString &filePath, QWebEngineDownloadItem::SavePageFormat format) const {
+    return savePage<QWebEngineDownloadItem>(mPage.data(), filePath, format);
 }
 
 #else
@@ -78,7 +130,7 @@ QCoro::Task<> QCoroWebEnginePage::save(const QString &filePath, QWebEngineDownlo
 namespace {
 
 QCoro::Task<QWebEngineLoadingInfo> handleLoadResult(QCoro::AsyncGenerator<QWebEngineLoadingInfo> &gen) {
-QCORO_FOREACH(const auto &info, changeGenerator) {
+    QCORO_FOREACH(const auto &info, changeGenerator) {
         switch (info.status()) {
         case QWebEngineLoadingInfo::LoadStartedStatus:
             continue;
@@ -93,9 +145,7 @@ QCORO_FOREACH(const auto &info, changeGenerator) {
 #elif defined(_MSVC_VER)
     __assume(false);
 #endif
-
 }
-
 
 } // namespace
 
@@ -111,4 +161,17 @@ QCoro::Task<QWebEngineLoadingInfo> QCoroWebEnginePage::load(const QWebEngineHttp
     return handleLoadResult(changeGenerator);
 }
 
+QCoro::Task<QByteArray> QCoroWebEnginePage::printToPdf(const QPageLayout &layout, const QPageRanges &ranges) {
+    WebEngineAwaitable<QByteArray> awaitable;
+    mPage->printToPdf([&awaitable](const auto &result) {
+        awaitable.resume(result);
+    }, layout, ranges);
+    co_return co_await awaitable;
+}
+
+QCoro::Task<> QCoroWebEnginePage::save(const QString &filePath, QWebEngineDownloadRequest::SavePageFormat saveFormat) const {
+    return savePage<QWebEngineDownloadRequest>(mPage.get(), filePath, saveFormat);
+}
+
 #endif
+
