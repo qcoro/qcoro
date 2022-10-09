23617010bf5f2ca0a0e2902937663caba8beb1c3
Jonah Brüchert <jbb@kaidan.im>
2022-10-09 22:57:34 +0200

    Add test for QmlTask

---
+++ tests/qcoroqmltask.cpp
@@ -0,0 +1,93 @@
+// SPDX-FileCopyrightText: 2022 Jonah Brüchert <jbb@kaidan.im>
+//
+// SPDX-License-Identifier: MIT
+
+#include "qcoroqml.h"
+#include "qcorotask.h"
+#include "qcorotimer.h"
+#include "qcoroqmltask.h"
+
+#include <QTest>
+#include <QTimer>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+class QmlObject : public QObject {
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE QCoro::QmlTask startTimer() {
+        auto *timer = new QTimer(this);
+        timer->setSingleShot(true);
+        timer->start(1s);
+        return [timer]() -> QCoro::Task<> {
+            co_await timer;
+        }();
+    }
+
+    Q_INVOKABLE void reportTestSuccess() {
+        Q_EMIT success();
+    }
+
+    Q_SIGNAL void success();
+};
+
+class QCoroQmlTaskTest : public QObject {
+    Q_OBJECT
+
+private:
+    Q_SLOT void testQmlCallback() {
+        QQmlApplicationEngine engine;
+        qmlRegisterSingletonType<QmlObject>("qcoro.test", 0, 1, "QmlObject", [](QQmlEngine *, QJSEngine *) {
+            return new QmlObject();
+        });
+
+        QCoro::Qml::registerTypes();
+
+        engine.loadData(R"(
+import qcoro.test 0.1
+import QtQuick 2.7
+
+QtObject {
+    Component.onCompleted: {
+        QmlObject.startTimer().then(() => {
+            console.log("Task JavaScript callback called")
+            QmlObject.reportTestSuccess()
+        })
+    }
+}
+)");
+        auto *object = engine.singletonInstance<QmlObject *>(qmlTypeId("qcoro.test", 0, 1, "QmlObject"));
+
+        auto *timeout = new QTimer(this);
+        timeout->setSingleShot(true);
+        timeout->setInterval(2s);
+        timeout->start();
+
+        bool running = true;
+        // End the event loop normally
+        connect(object, &QmlObject::success, this, [&]() {
+            timeout->stop();
+            running = false;
+        });
+        // Crash the test in case the timeout was reachaed without the callback being called
+        connect(timeout, &QTimer::timeout, this, [&]() {
+#if defined(__clang__) && defined(_WIN32)
+            QEXPECT_FAIL("", "QTBUG-91768", Abort);
+#endif
+            QFAIL("Timeout waiting for QML continuation to be called");
+
+        });
+        while (running) {
+            QCoreApplication::processEvents();
+        }
+    }
+};
+
+QTEST_GUILESS_MAIN(QCoroQmlTaskTest)
+
+#include "qcoroqmltask.moc"
