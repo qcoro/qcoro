dcceb07a2d634b987440ffdc05259c78228d806d
Daniel Vrátil <dvratil@kde.org>
2023-10-06 11:36:17 +0200

    Actions: update dbus binary download link for Windows CI

--- .github/actions/install-qt/install-dbus.ps1
+++ .github/actions/install-qt/install-dbus.ps1
@@ -3,6 +3,6 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name 'PSGallery' -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted
 Install-Module -Name 7Zip4PowerShell -Force
 
-Invoke-WebRequest -Uri https://files.kde.org/craft/master/22.05/windows/msvc2019_64/cl/RelWithDebInfo/libs/dbus/dbus-1.14.0-178-20220428T163337-windows-msvc2019_64-cl.7z -OutFile C:\Qt\dbus.7z
+Invoke-WebRequest -Uri https://files.kde.org/craft/master/23.09/windows/cl/msvc2019/x86_64/RelWithDebInfo/libs/dbus/dbus-1.14.8-20230912T142911-windows-cl-msvc2019-x86_64.7z -OutFile C:\Qt\dbus.7z
 Expand-7Zip -ArchiveFileName C:\Qt\dbus.7z -TargetPath $args[0]
 
